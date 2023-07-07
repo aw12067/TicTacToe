28e92019a8ea5b514c4be4de9c7d0a932ed0cca7
aw12067 <ashwinwalunj@gmail.com>
2023-07-07 15:30:39 +0530

    Finished implementing logic and added styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import './styles.scss'
 import { useState } from 'react'
 import Board from './components/Board'
 import { calculateWinner } from './winner'
-
+import StatusMessage from './components/StatusMessage'
 function App() {
   const [squares,setSquares] = useState(Array(9).fill(null))
   const[isXNext,setIsXNext] = useState(false);
 
   const winner = calculateWinner(squares);
-  const nextPlayer =  isXNext? 'X':'O';
-  const statusMessage = winner? `Winner is ${winner}`:`Next players is ${nextPlayer}`;
-
+  
     const handleSquareClick = clickedPosition =>{
       if(squares[clickedPosition] || winner){
         return;
@@ -27,11 +25,11 @@ function App() {
         setIsXNext (currentX =>{
             return !currentX
         })
-    };
+    }; 
     
   return (
     <div className='app'>
-      <h2>{statusMessage}</h2>
+      <StatusMessage winner={winner} isXNext={isXNext} squares={squares} />
       <Board squares={squares} handleSquareClick={handleSquareClick}/>
     </div>
   );
